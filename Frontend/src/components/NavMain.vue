0b08f9439e5f36b0d5bb318dfc317508b7bff83a
Thatphum.pk <thatphum.pk@gmail.com>
2022-04-28 06:00:30 +0700

    Edit Event

--- Frontend/src/components/NavMain.vue
+++ Frontend/src/components/NavMain.vue
@@ -6,8 +6,11 @@ const btn = () => {
 </script>
  
 <template>
-    <div class="grid grid-cols-2 flex items-center p-10 text-white">
-        <div class="text-2xl pl-10">จตุรเทพ</div>
+    <div class="grid grid-cols-2 flex items-center p-4 text-white">
+        <div class="flex items-center text-2xl pl-10">
+            <img src="../../public/xiao-white.png" alt="Logo" width="100" class="pt-1">
+            <p>จตุรเทพ</p>
+        </div>
         <div class="text-md flex justify-evenly">
             <router-link to="/" class="border-b-2">HOME</router-link>
             <router-link to="/about">DEV TEAM</router-link>
