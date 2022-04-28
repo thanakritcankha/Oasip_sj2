3215649b185c75660d7830dfe453cd122cc83956
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2022-04-29 02:40:19 +0700

    Frontend : Data Format

--- Frontend/src/components/Event.vue
+++ Frontend/src/components/Event.vue
@@ -1,17 +1,29 @@
-<script setup>
+<script setup></script>
 
-</script>
- 
 <template>
-    <div class="flex flex-col h-56 w-full bg-zinc-700 rounded-xl shadow-2xl shadow-zinc-900">
-        <div class="flex justify-end w-full h-8 bg-zinc-800 rounded-t-lg p-2">
-            <button class="h-4 w-4 bg-yellow-400 rounded-full mr-2"></button>
-            <button class="h-4 w-4 bg-green-500 rounded-full mr-2"></button>
-            <button class="h-4 w-4 bg-red-600 rounded-full mr-2"></button>
-        </div>
-        <div class="p-3">dsjakdjakl</div>
+  <div
+    class="flex flex-col h-56 w-full bg-zinc-700 rounded-xl shadow-2xl shadow-zinc-900"
+  >
+    <div class="flex justify-end w-full h-8 bg-zinc-800 rounded-t-lg p-2">
+      <button class="h-4 w-4 bg-yellow-400 rounded-full mr-2"></button>
+      <button class="h-4 w-4 bg-green-500 rounded-full mr-2"></button>
+      <button class="h-4 w-4 bg-red-600 rounded-full mr-2"></button>
     </div>
+    <!-- show data of booking -->
+    <div class="p-3">
+      <div class="eventsData">
+        <ul>
+          <h1>Date : 26-04-2022</h1>
+          <li>Booking name : Preeyathorn Chaiyakum</li>
+          <li>Start time : 00:00:00</li>
+          <li>Duration : 30 Minutes</li>
+          <li>Category : BackEnd Clinic</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
- 
+
 <style>
+
 </style>
