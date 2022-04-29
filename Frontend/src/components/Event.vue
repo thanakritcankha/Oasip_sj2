d4a13d2d29760ccc555f1f49a36477f8e2e48988
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2022-04-30 03:19:31 +0700

    add img

--- Frontend/src/components/Event.vue
+++ Frontend/src/components/Event.vue
@@ -26,12 +26,18 @@
           <li>Category : BackEnd Clinic</li>
         </ul> -->
         <ul class="tags">
-          <li><button class="tag">Backend Clinic</button></li>  
+          <li><button class="tag">Backend Clinic</button></li>
         </ul>
 
-        <h1 class="date">26-04-2022</h1>
-        <h1 class="duration">Duration : 30 Minutes</h1>
-        <h1 class="category">Category : BackEnd Clinic</h1>
+        <h1 class="date">
+          <img
+            class="icondate"
+            src="../../public/date-icon.png"
+            alt=""
+          />26-04-2022
+        </h1>
+        <h1 class="duration">30 Minutes</h1>
+        <!-- <h1 class="category">Category : BackEnd Clinic</h1> -->
         <h1 class="time">Start : 00:00:00</h1>
       </div>
     </div>
@@ -41,27 +47,23 @@
 <style>
 .name {
   flex: auto;
-
 }
 
 .name h1 {
   font-size: 1.5rem;
   font-weight: 600;
   color: #fff;
-
 }
 
 .eventsData {
   display: flex;
   flex-direction: column;
-
 }
 
 .eventsData ul {
   list-style: none;
   padding: 0;
   margin: 0;
-
 }
 
 .eventsData ul li {
@@ -69,31 +71,35 @@
   font-weight: 500;
   color: #fff;
   margin-bottom: 0.5rem;
-
+}
+.icondate {
+  width: 30px;
+  height: 30px;
+  margin-right: 0.5rem;
 }
 
 .date {
   font-size: 1.5rem;
   font-weight: 600;
   color: #fff;
-
+  border-bottom: 1px solid #fff;
+  padding-bottom: 0.5rem;
+  display: flex;
 }
 
 .tags {
   list-style: none;
   margin: 0;
-  overflow: hidden; 
+  overflow: hidden;
   padding: 0;
   width: auto;
   height: auto;
-
 }
 
 .tags li {
-  float: left; 
+  float: left;
   margin-right: 0.5rem;
   margin-bottom: 0.5rem;
-
 }
 
 .tag {
@@ -110,7 +116,6 @@
   transition: color 0.2s;
   width: auto;
   height: auto;
-
 }
 
 .tag::before {
@@ -123,7 +128,6 @@
   position: absolute;
   width: 6px;
   top: 10px;
-
 }
 
 .tag::after {
@@ -135,19 +139,16 @@
   position: absolute;
   right: 0;
   top: 0;
-
 }
 
 .tag:hover {
   background-color: #ddd;
   color: #444;
   cursor: pointer;
-
 }
 
 .tag:hover::after {
-   border-left-color: #ddd; 
-   
+  border-left-color: #ddd;
 }
 
 .time {
@@ -155,8 +156,13 @@
   font-weight: 500;
   color: #fff;
   margin-left: auto;
-
-
 }
 
+.duration {
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #fff;
+  margin-left: auto;
+  padding-bottom: 0.5rem;
+}
 </style>
