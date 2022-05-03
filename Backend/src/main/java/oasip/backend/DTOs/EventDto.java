b774a85561a8cd0af95d7c2c387a2277677997b2
Thatphum.pk <thatphum.pk@gmail.com>
2022-05-03 20:07:10 +0700

    edit time UTC in frontend and edit attibute localtime to instant in backend

--- Backend/src/main/java/oasip/backend/DTOs/EventDto.java
+++ Backend/src/main/java/oasip/backend/DTOs/EventDto.java
@@ -1,16 +1,19 @@
 package oasip.backend.DTOs;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class EventDto implements Serializable {
     private Integer id;
     private String bookingName;
     private String bookingEmail;
-    private LocalDateTime eventStartTime;
+    private Instant eventStartTime;
     private Integer eventDuration;
     private String eventNotes;
     private EventcategoryDto eventCategory;
